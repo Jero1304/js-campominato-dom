8ddfc2c12411ca11dec8fbfc19418c41a631df7f
Jero1304 <calogeroleto13@gmail.com>
2023-02-10 16:45:07 +0100

    FEAT genera e assegna bombe function

--- js/app.js
+++ js/app.js
@@ -1,30 +1,38 @@
 console.log('griglia');
 
-let latoGriglia = 8;
+let latoGriglia = 4;
 let numeroCelle = latoGriglia **2;
+let posizioneBombe = [];
 console.log('lato griglia:',latoGriglia,'numero celle:',numeroCelle);
-
 const grigliaEle = document.querySelector('.griglia');
 const gridBtnEle = document.getElementById('grid-btn');
 
 gridBtnEle.addEventListener('click', function(){
     
     grigliaEle.innerHTML = '';
+    scriviCelle(numeroCelle);
 
-    for (let i = 0; i < numeroCelle; i++) {
-        let num = i + 1;
-        
-        let divString = ` <div class="cella" style="width: calc(100% / ${latoGriglia});" >${num} </div> `;
-        grigliaEle.innerHTML += divString;
-    }
+    generaBombe(posizioneBombe);
+    
+    
+    
+    
     
     const cellaEle = document.querySelectorAll('.cella');
+
     for (let i = 0; i < cellaEle.length; i++) {
 
         let cellaSingola = cellaEle[i];
         cellaSingola.addEventListener('click', function(){
-            console.log(`cella numero ${i + 1}`);
-            cellaSingola.classList.add('bg-success');
+        numCel = i+1;
+            console.log(`cella numero ${numCel}`);
+
+            assegnaBombe(numCel,posizioneBombe,cellaSingola)
+
+            cellaSingola.classList.add('bg-success');               
+
+            
+
         })
     }
     
@@ -33,3 +41,36 @@ gridBtnEle.addEventListener('click', function(){
 
 
 
+/*******************
+FUNCTION
+********************/
+
+function scriviCelle(nCelle) {
+    for (let i = 0; i < nCelle; i++) {
+        let num = i + 1;
+        
+        let divString = ` <div class="cella" style="width: calc(100% / ${latoGriglia});" >${num} </div> `;
+        grigliaEle.innerHTML += divString;
+    }
+}
+
+function generaBombe(bombe){
+    let quanteBombe = latoGriglia * 2;
+        while (bombe.length < quanteBombe) {
+            
+            let num = Math.floor(Math.random()*numeroCelle + 1);
+            if (!bombe.includes(num)){
+                bombe.push(num);
+            }    
+        }
+        console.log(bombe);
+}
+
+function assegnaBombe(n,position,div) {
+    for (let i = 0; i < position.length; i++) {
+        if (n === position[i]) {
+            div.classList.add('bg-danger'); 
+            console.log('BOOM!');
+        }        
+    }
+}
